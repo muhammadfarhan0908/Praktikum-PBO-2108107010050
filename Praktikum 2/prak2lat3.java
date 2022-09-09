41504eaddd7826710fbae8ac263138905b9feed8
Muhammad Farhan <112547414+muhammadfarhan0908@users.noreply.github.com>
2022-09-09 21:23:55 +0700

    tugas nomor 3 praktikum 2 selesai

---
+++ Praktikum 2/prak2lat3.java
@@ -0,0 +1,12 @@
+public class prak2lat3{
+
+    public static void main(String arg[])
+{
+        for (int i=0; i<6; i++)
+    {
+        for (int j=6; j>i; j--)
+        System.out.print('*');
+        System.out.println();
+        }
+    }
+}  
