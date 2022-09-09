b08f49eaa9d55bd309f50fa79c25c9a222e40edd
Muhammad Farhan <112547414+muhammadfarhan0908@users.noreply.github.com>
2022-09-09 21:23:13 +0700

    tugas nomor 2 praktikum 2 selesai

---
+++ Praktikum 2/prak2lat2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class prak2lat2 {
+    public static void main(String []args )
+    {
+        Scanner a = new Scanner(System.in);
+        int nilai;
+        System.out.println("masukkan nilai anda");
+        nilai = a.nextInt();
+
+        if(nilai >= 87){
+            System.out.println("A");
+        }else if( nilai < 87 && nilai >= 78){
+            System.out.println("AB");
+        }else if( nilai < 78 && nilai >= 69){
+            System.out.println("B");
+        }else if( nilai < 69 && nilai >= 60){
+            System.out.println("BC");
+        }else if( nilai < 60 && nilai >= 51){
+            System.out.println("C");
+        }else if( nilai < 51 && nilai >= 41){
+            System.out.println("D");
+        }else {
+            System.out.println("E");
+        }
+        a.close();
+    }
+}
