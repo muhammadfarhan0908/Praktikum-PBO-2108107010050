f9d6c0443bbeef83fa5924ac15fd5ffd253e9fbd
Muhammad Farhan <112547414+muhammadfarhan0908@users.noreply.github.com>
2022-09-09 21:25:22 +0700

    tugas nomor 5 praktikum 2 selesai

---
+++ Praktikum 2/prak2lat5.java
@@ -0,0 +1,10 @@
+public class prak2lat5 {
+    public static void main(String arg[]) {
+        for (int i = 1; i < 21; i++) {
+            if(i%2==0 && i%6!=0)
+                System.out.print(i + " ");
+
+        }
+        
+    }
+}
