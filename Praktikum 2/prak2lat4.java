c2f227be2cfa8ab0801aae9217a142947ef9efb5
Muhammad Farhan <112547414+muhammadfarhan0908@users.noreply.github.com>
2022-09-09 21:24:36 +0700

    tugas nomor 4 praktikum 2 selesai

---
+++ Praktikum 2/prak2lat4.java
@@ -0,0 +1,10 @@
+public class prak2lat4 {
+    public static void main(String arg[]) {
+        for (int i = 1; i < 21; i++) {
+            if(i%2==0)
+                System.out.println(i);
+
+        }
+        
+    }
+}
