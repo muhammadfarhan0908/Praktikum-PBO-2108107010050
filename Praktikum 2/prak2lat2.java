8d66ad0b053ce08d74862b18b5eb0ed9b50cb633
Muhammad Farhan <112547414+muhammadfarhan0908@users.noreply.github.com>
2022-09-12 08:28:31 +0700

    Update prak2lat2.java

--- Praktikum 2/prak2lat2.java
+++ Praktikum 2/prak2lat2.java
@@ -20,9 +20,11 @@ public class prak2lat2 {
             System.out.println("C");
         }else if( nilai < 51 && nilai >= 41){
             System.out.println("D");
-        }else {
+        }else if (nilai<41){
             System.out.println("E");
-        }
+        }else{
+            System.out.println("invalid!");
         a.close();
+        }
     }
 }
